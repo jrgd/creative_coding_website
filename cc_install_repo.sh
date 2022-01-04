23d5f43bbce4a02367c6e5cf696c0cb342ba6e59
jrgd <jrgd@mac.mini.studio>
2022-01-04 10:56:54 -0500

    use specific local folder on local machine to gather content for the ./source_notes content to be published

--- cc_install_repo.sh
+++ cc_install_repo.sh
@@ -2,3 +2,8 @@
 rm -r ./source
 mkdir ./source
 php install_repositories.php
+
+# copy files from the SyncThing folder, made specifically to publish content
+rm -r ./source_notes
+mkdir ./source_notes
+cp -r ~/Sync_thinking-doing-writing/publish/creative_coding_website ./source_notes
