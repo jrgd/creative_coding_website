558f26934badcef26cd73f8b9cf212b46c11eb10
jrgd <jrgd@mac.mini.studio>
2022-01-04 11:04:59 -0500

    copy the files inside the folder; not the folder itself

--- cc_install_repo.sh
+++ cc_install_repo.sh
@@ -6,4 +6,4 @@ php install_repositories.php
 # copy files from the SyncThing folder, made specifically to publish content
 rm -r ./source_notes
 mkdir ./source_notes
-cp -r ~/Sync_thinking-doing-writing/publish/creative_coding_website ./source_notes
+cp -r ~/Sync_thinking-doing-writing/publish/creative_coding_website/* ./source_notes
