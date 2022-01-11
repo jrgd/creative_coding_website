6274d15666e13060a6bce0ce082f41c971db41ed
jrgd <jrgd@mac.mini.studio>
2022-01-11 14:37:12 -0500

    new 51 kanban macos; rebuild website

--- install_repositories.php
+++ install_repositories.php
@@ -36,6 +36,7 @@ $repositories_array = [
   "https://github.com/jrgd/creative_coding_042",
   "https://github.com/jrgd/creative_coding_043_clui",
   "https://github.com/jrgd/creative_coding_050",
+  "https://github.com/jrgd/creative_coding_051_kanban",
 ];
 
 foreach($repositories_array as $single_repository) {
