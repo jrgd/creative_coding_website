37fabe8b878e2539b783f3291a48a93e7b2dced9
jrgd <jrgd@mac.mini.studio>
2022-01-12 23:19:14 -0500

    fix publishing of 51 (no index.html); rebuild website and fix W permission to rm ./source

--- cc_install_repo.sh
+++ cc_install_repo.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+chmod -R +w ./source
 rm -r ./source
 mkdir ./source
 php install_repositories.php
--- cc_publish.sh
+++ cc_publish.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+"creativecoding.xyz" > ./docs/CNAME
 yarn build
 git commit -m 'publish: website automatically rebuilt'
 git push
