fe5c0235ee7bcc43f5ca7826f669ec1097ad35ab
jrgd <jrgd@mac.mini.studio>
2022-03-08 23:21:43 -0500

    the path; blah. fixing broken links for navigation after removing the <base> url

--- cc_directory_parse.php
+++ cc_directory_parse.php
@@ -137,7 +137,7 @@ if (is_dir($dir)) {
                 $presentation_data .= $Parsedown->text($readme_content);
                 $presentation_data .= "<br>";
                 if ($file_inside == "index.html" || $file_inside ==  "index.php") {
-                  $presentation_data .= "<a href='{$file}/{$file_inside}'>View the experiment</a><br>";
+                  $presentation_data .= "<a href='./{$file_inside}'>View the experiment</a><br>";
                 }
                 $presentation_data .= "<a href='".file_get_contents("./source/{$file}/repository_link.txt")."'>Git repository</a>";
                 $presentation_data .= "</p>";
